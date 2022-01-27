04a5c443dbbea88ddf032339a5de8755a33e6c0d
Harsh Goyal <harsh.goyal.cd.eee20@itbhu.ac.in>
2022-01-27 15:58:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const words = ["Web Devloper", "Competetive Programmer", "IITian","Electrical Engineer"];
+
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('typed').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 100);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('typed').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 100);
+	};
+	loopDeleting();
+};
+
+typingEffect();
